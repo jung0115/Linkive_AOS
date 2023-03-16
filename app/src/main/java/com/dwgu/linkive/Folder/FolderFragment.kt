975241780191628d0baae3c84b3e3961c8d9dfdb
sumin <gosumin0710@naver.com>
2023-03-16 23:30:38 +0900

    issue #9 feature: 폴더 정렬 스피너 구현 -> 폴더 리스트 구현하기

--- app/src/main/java/com/dwgu/linkive/Folder/FolderFragment.kt
+++ app/src/main/java/com/dwgu/linkive/Folder/FolderFragment.kt
@@ -5,56 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.dwgu.linkive.Folder.sortFolder.sortFolderAdapter
 import com.dwgu.linkive.R
+import com.dwgu.linkive.databinding.FragmentFolderBinding
+import kotlinx.android.synthetic.main.fragment_folder.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FolderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FolderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentFolderBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_folder, container, false)
+        _binding = FragmentFolderBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FolderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FolderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 폴더 정렬 스피너 어댑터 연결
+//        val sortFolderAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.item_sort_folder, android.R.layout.simple_spinner_dropdown_item)
+//        binding.spinnerSortFolder.adapter = sortFolderAdapter
+        val data: ArrayList<String> = arrayListOf()
+        data.add("생성순")
+        data.add("가나다순")
+        val sortFolderAdapter = sortFolderAdapter(requireContext(), data)
+        binding.spinnerSortFolder.adapter = sortFolderAdapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/dwgu/linkive/Folder/sortFolder/sortFolderAdapter.kt
@@ -0,0 +1,68 @@
+package com.dwgu.linkive.Folder.sortFolder
+
+import android.R
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.dwgu.linkive.databinding.ItemSortFolderDropdownBinding
+import com.dwgu.linkive.databinding.ItemSortFolderNormalBinding
+
+
+class sortFolderAdapter(context: Context, var data: List<String>) : BaseAdapter() {
+
+
+    //    var context: Context? = null
+//    var data: List<String>? = null
+    var inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+//    fun sortFolderAdapter(context: Context, data: List<String>) {
+//        this.context = context
+//        this.data = data
+//        inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//    }
+
+    override fun getCount(): Int {
+        return if (data != null) data.size;
+        else 0;
+    }
+
+    override fun getItem(position: Int): Any {
+        return data[position];
+    }
+
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+//        if (convertView == null) {
+//            convertView = inflater.inflate(R.layout.simple_spinner_dropdown_item, parent, false)
+//        }
+        val binding = ItemSortFolderNormalBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val model = data[position]
+        try {
+            binding.spinnerText.text = model
+
+        }catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return binding.root
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding = ItemSortFolderDropdownBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val model = data[position]
+        try {
+            binding.spinnerText.text = model
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return binding.root
+    }
+
+}
