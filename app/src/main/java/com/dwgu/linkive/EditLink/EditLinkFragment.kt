d9e42010871c740538a442c9b80e60ab473ffbcc
jung0115 <jungim7490@gmail.com>
2023-03-30 21:55:27 +0900

    issue #2 feature: 링크 편집 페이지 - 상단 완료

--- app/src/main/java/com/dwgu/linkive/EditLink/EditLinkFragment.kt
+++ app/src/main/java/com/dwgu/linkive/EditLink/EditLinkFragment.kt
@@ -5,12 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.dwgu.linkive.R
 import com.dwgu.linkive.databinding.FragmentEditLinkBinding
 
 class EditLinkFragment : Fragment() {
 
     // ViewBinding Setting
     lateinit var binding: FragmentEditLinkBinding
+
+    // PageSheet 선택 Spinner
+    // PageSheet 리스트
+    private val pagesheetList = mutableListOf<String>()
+    // Spinner 어댑터
+    private var pagesheetAdapter: ArrayAdapter<String>? = null
+    // 선택된 PageSheet
+    private var selectPagesheet: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +34,31 @@ class EditLinkFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // PageSheet 선택 Spinner
+        // 기본 PageSheet
+        pagesheetList.add(getString(R.string.pagesheet_free))
+        pagesheetList.add(getString(R.string.pagesheet_trip))
+        pagesheetList.add(getString(R.string.pagesheet_study))
+        pagesheetList.add(getString(R.string.pagesheet_programming))
+        pagesheetList.add(getString(R.string.pagesheet_diary))
+        pagesheetList.add(getString(R.string.pagesheet_checklist))
+        // Custom PageSheet - 샘플 데이터
+        pagesheetList.add("페이지시트1")
+        pagesheetList.add("페이지시트2")
+        pagesheetAdapter = ArrayAdapter(requireContext(), R.layout.item_spinner_select_pagesheet, pagesheetList)
+        binding.spinnerSelectPagesheet.adapter = pagesheetAdapter
+
+        // PageSheet 선택 Spinner 누르면 PageSheet 선택 리스트 나타남
+        binding.spinnerSelectPagesheet.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // 선택된 PageSheet
+                selectPagesheet = binding.spinnerSelectPagesheet.getSelectedItem().toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
     }
 }
--- app/src/main/java/com/dwgu/linkive/LinkView/LinkViewBottomSheet/ManageLinkBottomFragment.kt
+++ app/src/main/java/com/dwgu/linkive/LinkView/LinkViewBottomSheet/ManageLinkBottomFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dwgu.linkive.EditLink.EditLinkFragment
+import com.dwgu.linkive.R
 import com.dwgu.linkive.databinding.FragmentManageLinkBottomBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -24,6 +26,19 @@ class ManageLinkBottomFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // 페이지 수정 버튼 선택 시 -> 링크 편집 페이지
+        binding.relativelayoutEditPage.setOnClickListener(View.OnClickListener {
+            // 현재 BottomSheet 닫기
+            dismiss()
+
+            // 링크 편집 페이지 열기
+            requireActivity()
+                .supportFragmentManager
+                .beginTransaction()
+                .add(R.id.nav_host_fragment, EditLinkFragment())
+                .commit()
+        })
 
         // 폴더 이동 버튼 선택 시 -> 폴더 선택 BottomSheet
         binding.relativelayoutMoveFolder.setOnClickListener(View.OnClickListener {
